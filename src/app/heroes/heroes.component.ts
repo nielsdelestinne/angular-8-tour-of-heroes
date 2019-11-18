4c5c640cc2ea8360da988fc40424b3b48a46b4d1
nielsdelestinne <nielsdelestinne@gmail.com>
2019-11-18 20:37:49 +0100

    4. Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from './hero.model';
 import {HEROES} from './heroes.mock';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,11 +12,17 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  private getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(singleHero: Hero): void {
--- src/app/heroes/heroes.mock.ts
+++ src/app/heroes/heroes.mock.ts
@@ -7,4 +7,5 @@ export const HEROES: Hero[] = [
   {id: 4, name: 'Jimmy Jarule Evil'},
   {id: 5, name: 'Silvester Savior Evil'},
   {id: 6, name: 'Betty Bestastic Evil'},
+  {id: 7, name: 'Tinki Wonka'},
 ];
