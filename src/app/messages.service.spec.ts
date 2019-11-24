159978f165b8b4f5ba7b0c4ebef7d1ca1c685efe
nielsdelestinne <nielsdelestinne@gmail.com>
2019-11-24 10:18:58 +0100

    spec for messageService

--- src/app/messages.service.spec.ts
+++ src/app/messages.service.spec.ts
@@ -3,10 +3,37 @@ import { TestBed } from '@angular/core/testing';
 import { MessagesService } from './messages.service';
 
 describe('MessagesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let messageService: MessagesService;
+
+  beforeEach(() => {
+    messageService = new MessagesService();
+    TestBed.configureTestingModule({});
+  });
 
   it('should be created', () => {
     const service: MessagesService = TestBed.get(MessagesService);
     expect(service).toBeTruthy();
+  });
+
+  it('should allow to add a message', () => {
+    const service: MessagesService = TestBed.get(MessagesService);
+    service.add('This is my new message');
+
+    expect(service.messages).toContain('This is my new message');
+    expect(service.messages.length).toEqual(1);
+
+  });
+
+  it('should be created (no testbed)', () => {
+    expect(messageService).toBeTruthy();
+  });
+
+  it('should allow to add a message (no testbed)', () => {
+    messageService.add('This is my new message');
+
+    expect(messageService.messages).toContain('This is my new message');
+    expect(messageService.messages.length).toEqual(1);
+
   });
 });
